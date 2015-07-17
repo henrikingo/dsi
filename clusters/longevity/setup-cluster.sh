b70946edeefb80ad0ab441bb7900b6f982d55aeb
Rui Zhang <rui.zhang@mongodb.com>
2015-07-17 12:46:07 -0700

    mute longevity terraform output again

--- clusters/longevity/setup-cluster.sh
+++ clusters/longevity/setup-cluster.sh
@@ -3,10 +3,10 @@
 cp ../../terraform/* .
 
 # create all resources and instances
-./terraform apply -var="count=3"
+./terraform apply -var="count=3" > terraform.log
 
 # workaround for failure to bring up all at the same time
-./terraform apply -var="count=9"
+./terraform apply -var="count=9" >> terraform.log
 
 # this will extract all public and private IP address information
 ./env.sh
