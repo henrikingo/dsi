d001e0c2fd6c2e0e2768cc7ad2e8efb6bebc99e7
Rui Zhang <rui.zhang@mongodb.com>
2015-10-01 14:46:58 -0700

    CAP-2512 fix a merge error

--- clusters/replica/cluster.tf
+++ clusters/replica/cluster.tf
@@ -198,7 +198,7 @@ resource "aws_instance" "master" {
             "sudo /usr/sbin/alternatives --install /usr/bin/java java /usr/java/jdk1.7.0_71/bin/java 20000",
             "wget --quiet --no-check-certificate http://central.maven.org/maven2/org/mongodb/mongo-java-driver/2.13.0/mongo-java-driver-2.13.0.jar",
             "echo 'export CLASSPATH=~/mongo-java-driver-2.13.0.jar:$CLASSPATH' >> ~/.bashrc",
-            "cd ~; wget --quiet --no-check-certificate https://s3-us-west-2.amazonaws.com/dsi-donot-remove/ycsb/YCSB.tar.gz; tar zxvf YCSB.tar.gz"
+            "cd ~; wget --quiet --no-check-certificate https://s3-us-west-2.amazonaws.com/dsi-donot-remove/ycsb/YCSB.tar.gz; tar zxvf YCSB.tar.gz",
             "curl https://s3-us-west-2.amazonaws.com/dsi-donot-remove/utils/install_maven.sh | sudo bash",
             "source /etc/profile.d/maven.sh; cd /home/ec2-user/YCSB/ycsb-mongodb; ./setup.sh > /dev/null",
             "mkdir -p ~/hammer; cd ~/hammer; wget --no-check-certificate https://raw.githubusercontent.com/rzh/hammer.mongo/master/scripts/bootstrap.sh -O - | bash",
