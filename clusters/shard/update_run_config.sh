9516707958a1732407e0abb62fbe7a08dbd83323
Rui Zhang <rui.zhang@mongodb.com>
2015-10-09 13:06:15 -0700

    CAP-2598 fix instances for monitoring

--- clusters/shard/update_run_config.sh
+++ clusters/shard/update_run_config.sh
@@ -3,6 +3,6 @@
 source setting.sh
 
 sed -i -- "s/%%MS%%/\"$SSHUSER@$ms\"/g" *.json
-sed -i -- "s/%%ALLMEMBERS%%/\"$SSHUSER@$p1\", \"$SSHUSER@$p2\", \"$SSHUSER@$p3\", \"$SSHUSER@$p4\", \"$SSHUSER@$p5\", \"$SSHUSER@$p6\", \"$SSHUSER@$p7\", \"$SSHUSER@$p8\", \"$SSHUSER@$p9\", \"$SSHUSER@$p10\", \"$SSHUSER@$p11\", \"$SSHUSER@$p12\"/g" *.json
+sed -i -- "s/%%ALLMEMBERS%%/\"$SSHUSER@$p1\", \"$SSHUSER@$p2\", \"$SSHUSER@$p3\", \"$SSHUSER@$p4\", \"$SSHUSER@$p5\", \"$SSHUSER@$p6\", \"$SSHUSER@$p7\", \"$SSHUSER@$p8\", \"$SSHUSER@$p9\"/g" *.json
 sed -i -- "s/%%MS_PRIVATE_IP%%/$ms_private_ip/g" *.json
 sed -i -- "s/%%CLIENT%%/\"$SSHUSER@$mc\"/g" *.json
