d608077b05873fa38640d66d20024262f613782a
Kyle Suarez <ksuarz@gmail.com>
2015-10-26 17:40:11 -0400

    PERF-226 streamline override generation process
    This new override runner script will auto-magically generate new overrides for
    you. Run it with `--help` to see its usage.
    
    It takes as input a revision ID, a tag to compare against and a JIRA ticket for
    tracking purposes.

--- update_overrides.py
+++ update_overrides.py
@@ -1,50 +1,60 @@
-#!/usr/bin/env python
-import json
-import requests
-import perf_regression_check
+# Copyright 2015 MongoDB Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Runner script for updating overrides."""
+
+import argparse
+
+import override
 
 
-tests = ["Where.SimpleNested.Where", "Where.ComplexNested", "Where.ReallyBigNestedComparison.Where"]
-variants = ['linux_wt_standalone']
-tag = '3.1.8-Baseline'
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='update-overrides',
+                                     description='Update performance test overrides')
+    parser.add_argument('revision_id',
+                        help='The Evergreen ID of the desired revision')
+    parser.add_argument('tag',
+                        help='The tag to compare against (e.g. "3.1.8-Baseline")')
+    parser.add_argument('ticket',
+                        help='The JIRA ticket associated with this override update')
+    parser.add_argument('-f',
+                        '--override-file',
+                        default='override.json',
+                        help='The path to the override file to update')
+    parser.add_argument('-v',
+                        '--variants',
+                        help='The variant or list of variants to update')
+    parser.add_argument('--tasks',
+                        help='The task or list of tasks to update')
+    parser.add_argument('-t',
+                        '--tests',
+                        help='The test or list of tests to update')
+    parser.add_argument('-u',
+                        '--evergreen-url',
+                        dest='evg_url',
+                        help='The URL of the Evergreen server')
+    parser.add_argument('-d',
+                        '--destination-file',
+                        default='override.json',
+                        help='The path to write the updated override')
 
+    # Parse the arguments and find the override file
+    kwargs = parser.parse_args()
+    ovr = override.Override(kwargs.override_file)
 
-base_url = 'https://evergreen.mongodb.com/api/2/task/performance_'
-# Replace this githash with a call to get most recent
-githashplus = 'e5bd1ecd48d78900bafd64022200f94eb7be24c7_15_09_25_05_37_53'
-githash = 'e5bd1ecd48d78900bafd64022200f94eb7be24c7'
+    # Pass the rest of the command-line arguments to Override.update
+    ovr.update(**kwargs.__dict__)
 
-
-replTasks = ['insert', 'update', 'misc', 'singleThreaded']
-standTasks = ['insert', 'update', 'misc', 'singleThreaded', 'query', 'where', 'geo']
-if 'linux_wt_repl' in variants or 'linux_mmap_repl' in variants : 
-    tasks = replTasks
-else : 
-    tasks = standTasks
-
-# Read in the original override.json file
-overrides = json.load(open('../etc/override.json'))
-
-# For each variant
-for variant in variants:
-    variantData = []
-# Pull down the history data for that variant for all task types
-    for task in tasks:
-        Variant = variant.replace('_', '-')
-        variantTask = variant + "_" + task + "_"
-        print "Task is %s" % task
-        print "Getting URL: %s" % base_url + variantTask + githashplus + '/json/tags/' + task + '/perf'
-        r = requests.get(base_url + variantTask + githashplus + '/json/tags/' + task
- + '/perf')
-        variantData.extend(r.json())
-        
-    # For each test name
-    # Lookup entry for that in the histories, and replace in overrides
-    tagHistory = perf_regression_check.History(variantData)
-    for test in tests : 
-        print "overriding overrides[%s]['reference'][%s]" % (Variant, test)
-        overrides[Variant]["reference"][test] = tagHistory.seriesAtTag(test, tag)
-
-# Write out new overrides
-json.dump(overrides, open('../etc/override.json', 'w'), indent=4, separators=(',',':'), sort_keys=True)
-
+    # Dump the new file as JSON
+    ovr.save_to_file(kwargs.destination_file)
