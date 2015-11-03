cc8f2629ebaaa0ae114ffcf482de469a8be24a99
Kyle Suarez <kyle.suarez@mongodb.com>
2015-11-03 11:19:07 -0500

    Update evergreen module
    This rehauls the Evergreen module into something simpler and more efficient.
    First, all of the default REST endpoints have been wrapped in "query_*" methods,
    which simply hits the endpoint, retrieves the information and transforms the
    JSON response into a Python dict. It handles all of the HTTP requests and JSON
    transformations transparently.
    
    Second, most of the functions that involve the use of the endpoint data have
    been reworked into generators. They return the most useful information: the name
    of the build variant, task or test, along with the corresponding Evergreen ID.
    
    A new endpoint has been added to get performance task information for the
    performance project.
    
    Renamed `evergreen.EvergreenClient` to `evergreen.Client` for simplicity.
    
    `evergreen.Empty` is a subclass of `evergreen.EvergreenError` to be both more
    concise and more descriptive.

--- evergreen.py
+++ evergreen.py
@@ -15,12 +15,12 @@
 """Module for interacting with Evergreen."""
 
 # TODO: Replace print statements with the logging module. In PyCharm, it doesn't seem to work with the console
-# TODO: Code cleanup
 
 from __future__ import print_function
 import logging
 
 import helpers
+
 
 DEFAULT_EVERGREEN_URL = 'https://evergreen.mongodb.com'
 """The default Evergreen URL."""
@@ -31,22 +31,167 @@ class EvergreenError(Exception):
     pass
 
 
-class EvergreenClient(object):
-    """Allows for interaction with an Evergreen server."""
+class Empty(EvergreenError):
+    """Indicates that an empty response from Evergreen was not expected."""
+    pass
 
-    def __init__(self, base_url=None, verbose=True):
+
+class Client(object):
+    """Allows for interaction with an Evergreen server.
+
+    This class has two sets of methods. The first type are the "query" functions, which are simple wrappers around the
+    Evergreen JSON endpoints. They handle making the request and return the output unchanged as a JSON-compatible
+    dictionary.
+
+    The second group builds on the query functions, and they return useful information about Evergreen revisions, build
+    variants and tasks by extracting the relevant information. Most of these are implemented as generators to cache
+    the results of the HTTP queries.
+    """
+
+    def __init__(self, configuration=None, verbose=True):
         """Create a new handle to an Evergreen server.
 
-        :param base_url: (optional) The URL to the Evergreen server. If not specified, uses a reasonable default.
+        :param str|file configuration: (optional) A personal YAML configuration for Evergreen
+        :param bool verbose: (optional) Control the verbosity of logging statements
         """
         self.logger = logging.getLogger('evergreen')
         self.logger.level = logging.INFO if verbose else logging.WARNING
-        self.base_url = base_url if base_url is not None else DEFAULT_EVERGREEN_URL
+
+        # Parse the config file
+        try:
+            yml = helpers.file_as_yaml(configuration)
+            self.headers = {
+                'auth-username': yml['user'],
+                'api-key': yml['api_key']
+            }
+            self.base_url = yml['ui_server_host']
+        except (TypeError, KeyError):
+            self.base_url = DEFAULT_EVERGREEN_URL
+            self.headers = {}
+
+    def query_project_history(self, project):
+        """Gets all the information on the most recent revisions.
+
+        Evergreen endpoint: /rest/v1/projects/{project_name}/versions
+
+        :param str project: The name of the Evergreen project (e.g. 'performance', 'sys-perf')
+        :rtype: dict
+        """
+        return helpers.get_as_json(
+            '{url}/rest/v1/projects/{project_id}/versions'.format(url=self.base_url, project_id=project),
+            headers=self.headers
+        )
+
+    def query_revision_by_commit(self, project, revision):
+        """Get Evergreen run information on a given Git commit.
+
+        Evergreen endpoint: /rest/v1/projects/{project_id}/revisions/{revision}
+
+        :param str project:  The name of the Evergreen project (e.g. 'performance', 'sys-perf')
+        :param str revision: The SHA1 of the desired Git commit
+        :rtype: dict
+        """
+        return helpers.get_as_json(
+            '{url}/rest/v1/projects/{project_id}/revisions/{revision}'.format(url=self.base_url,
+                                                                              project_id=project,
+                                                                              revision=revision),
+            headers=self.headers
+        )
+
+    def query_revision(self, revision_id):
+        """Get information on a given revision.
+
+        Evergreen endpoint: /rest/v1/versions/{revision_id}
+
+        :param str revision_id: The Evergreen ID of a particular revision or version
+        :rtype: dict
+        """
+        return helpers.get_as_json(
+            '{url}/rest/v1/versions/{version_id}'.format(url=self.base_url, version_id=revision_id),
+            headers=self.headers
+        )
+
+    def query_revision_status(self, revision_id):
+        """Get the status of a particular revision.
+
+        Evergreen endpoint: /rest/v1/versions/{revision_id}/status
+
+        :param str revision_id: The Evergreen ID of a particular revision or version
+        :rtype: dict
+        """
+        return helpers.get_as_json(
+            '{url}/rest/v1/versions/{version_id}/status'.format(url=self.base_url, version_id=revision_id),
+            headers=self.headers
+        )
+
+    def query_build_variant(self, build_variant_id):
+        """Get information on a particular build variant.
+
+        Evergreen endpoint: /rest/v1/builds/{build_id}
+
+        :param str build_variant_id: The Evergreen ID of a particular build variant
+        :rtype: dict
+        """
+        return helpers.get_as_json(
+            '{url}/rest/v1/builds/{build_id}'.format(url=self.base_url, build_id=build_variant_id),
+            headers=self.headers
+        )
+
+    def query_build_variant_status(self, build_variant_id):
+        """Get the status of a particular build variant.
+
+        Evergreen endpoint: /rest/v1/builds/{build_id}/status
+
+        :param str build_variant_id: The Evergreen ID of a particular build variant
+        :rtype: dict
+        """
+        return helpers.get_as_json(
+            '{url}/rest/v1/builds/{build_id}/status'.format(url=self.base_url, build_id=build_variant_id),
+            headers=self.headers
+        )
+
+    def query_task(self, task_id):
+        """Get information on a particular task.
+
+        Evergreen endpoint: /rest/v1/tasks/{task_id}
+
+        :param str task_id: The Evergreen ID of a particular task
+        :rtype: dict
+        """
+        return helpers.get_as_json(
+            '{url}/rest/v1/tasks/{task_id}'.format(url=self.base_url, task_id=task_id),
+            headers=self.headers
+        )
+
+    def query_task_status(self, task_id):
+        """Get the status of a particular task
+
+        Evergreen endpoint: /rest/v1/tasks/{task_id}/status
+
+        :param str task_id: The Evergreen ID of a particular task
+        :rtype: dict
+        """
+        return helpers.get_as_json(
+            '{url}/rest/v1/tasks/{task_id}/status'.format(url=self.base_url, task_id=task_id),
+            headers=self.headers
+        )
+
+    def query_task_history(self, task_name):
+        """Get the history of a particular task.
+
+        Evergreen endpoint: /rest/v1/tasks/{task_name}/history
+
+        :param str task_name: The Evergreen ID of a particular task
+        :rtype: dict
+        """
+        return helpers.get_as_json(
+            '{url}/rest/v1/tasks/{task_name}/history'.format(url=self.base_url, task_name=task_name)
+        )
 
     def get_recent_revisions(self, project_name, max_results=10):
         """Get the most recent revisions for an Evergreen project.
 
-        Valid project names include "performance" for the MongoDB Perf Project, "sys_perf" for System Performance, and
+        Valid project names include "performance" for the MongoDB Perf Project, "sys-perf" for System Performance, and
         so on.
 
         This method returns a list of dicts, where each dict describes the version and the various build variants
@@ -64,106 +209,99 @@ class EvergreenClient(object):
         :return: A list containing build information on the most recent commits
         :rtype: list[dict]
         """
-        response = helpers.get_as_json('{0}/rest/v1/projects/{1}/versions'.format(self.base_url, project_name))
-        return response['versions'][:max_results]
+        return self.query_project_history(project_name)['versions'][:max_results]
 
-    def get_build_variants_from_git_commit(self, project, commit_sha):
-        """Get the build variants associated with the given project and Git commit SHA1.
+    def query_task_perf_tags(self, task_name, task_id):
+        """Get the tag data of a particular task in the Performance project.
 
-        :param project: The Evergreen project ID
-        :param commit_sha: The SHA1 of the desired Git commit
-        :return: A list of build variant IDs
-        :rtype: list[str]
+        This works for the performance project. The sys-perf and longevity projects may return empty responses; use
+        a different method instead.
+
+        Evergreen endpoint: /api/2/task/{task_id}/json/tags/{task_name}/perf
+
+        :param str task_name: The common name of the task
+        :param str task_id: The Evergreen ID of a particular task
+        :rtype: list
+        """
+        return helpers.get_as_json(
+            '{url}/api/2/task/{task_id}/json/tags/{task_name}/perf'.format(url=self.base_url,
+                                                                           task_id=task_id,
+                                                                           task_name=task_name),
+            headers=self.headers
+        )
+
+    def build_variants_from_git_commit(self, project, commit_sha):
+        """Generates the names and Evergreen IDs of build variants.
+
+        Build variant names have underscores replaced with hyphens, to aid in
+
+        :param str project: The name of the Evergreen project
+        :param str commit_sha: The SHA1 of the Git commit
+        :return: Tuples in the form ("build-variant-name", "build-variant-id")
+        :rtype: tuple(str, str)
         """
         response = helpers.get_as_json('{0}/rest/v1/projects/{1}/revisions/{2}'.format(self.base_url,
                                                                                        project,
-                                                                                       commit_sha))
-        return response['builds']
+                                                                                       commit_sha),
+                                       headers=self.headers)
+        names = sorted([x.replace('_', '-') for x in response['build_variants']])
+        ids = sorted(response['builds'])
+        for item in zip(names, ids):
+            yield item
 
-    def get_build_variants_from_revision_id(self, revision_id):
-        """Get the build variants associated with with this project revision.
+    def build_variants_from_revision_id(self, revision_id):
+        """Generates the names and Evergreen IDs of build variants.
 
-        This returns a list of build variant IDs from Evergreen. With this ID, you can find the specific tasks
-        associated with that particular variant.
-
-        :param revision_id: The revision ID in Evergreen
-        :return: A list of build variant IDs
-        :rtype: list[str]
+        :param str revision_id: The ID of the revision in Evergreen
+        :return: Tuples in the form ("build-variant-name", "build-variant-id")
+        :rtype: tuple(str, str)
         """
-        response = helpers.get_as_json('{0}/rest/v1/versions/{1}'.format(self.base_url, revision_id))
-        return response['builds']
+        response = helpers.get_as_json('{0}/rest/v1/versions/{1}'.format(self.base_url, revision_id),
+                                       headers=self.headers)
+        names = sorted([x.replace('_', '-') for x in response['build_variants']])
+        ids = sorted(response['builds'])
+        for item in zip(names, ids):
+            yield item
 
-    def get_build_variant_names_from_git_commit(self, project, commit):
-        """Get the build variant names associated with this project and Git revision.
+    def tasks_from_build_variant(self, build_variant_id):
+        """Generates the names and Evergreen IDs of the tasks associated with this build variant.
 
-        This returns a list of variant names in human-readable format. Underscores received from the server are
-        automatically replaced with hyphens, to aid in querying for other Evergreen information.
-
-        :param project: The Evergreen project ID
-        :param commit: The SHA1 of the desired Git commit
-        :return: A list of build variant names
-        :rtype: list[str]
+        :param str build_variant_id: The ID of the build variant in Evergreen
+        :return: Tuples in the form ("task-name", "task-id")
+        :rtype: tuple(str, str)
         """
-        response = helpers.get_as_json('{0}/rest/v1/projects/{1}/revisions/{2}'.format(self.base_url, project, commit))
-        return [variant.replace('_', '-') for variant in response['build_variants']]
+        build_info = self.query_build_variant(build_variant_id)
+        tasks = build_info['tasks']
+        for task_name in tasks.keys():
+            yield (task_name, tasks[task_name]['task_id'])
 
-    def get_build_variant_names_from_revision_id(self, revision_id):
-        """Get the build variant names associated with this project version.
+    def tests_from_task(self, task_id):
+        """Generates the names and status information of the tests associated with this task.
 
-        This returns a list of variant names in human-readable format. Underscores received from the server are
-        automatically replaced with hyphens, to aid in querying for other Evergreen information.
+        The test status information looks something like this:
+        {
+            'logs': {'url': '...'},
+            'status': 'pass',
+            'time_taken': 123123123
+        }
 
-        :param str revision_id: The version ID in Evergreen
-        :return: A list of build variant names
-        :rtype: list[str]
+        :param str task_id: The ID of the task in Evergreen
+        :rtype: tuple(str, dict)
         """
-        response = helpers.get_as_json('{0}/rest/v1/versions/{1}'.format(self.base_url, revision_id))
-        return [variant.replace('_', '-') for variant in response['build_variants']]
+        task_info = self.query_task(task_id)
+        for test_name in task_info['test_results'].keys():
+            yield (test_name, task_info['test_results'][test_name])
 
-    def get_tasks_from_build_variant(self, build_variant_id):
-        """Get the tasks associated with this build ID.
+    def failed_tests_from_task(self, task_id):
+        """Generates the tests that have failed for this task.
 
-        This returns a JSON-compatible dict describing the status of the tasks. The keynames are the names of each task;
-        the values are subdocuments with more information. For example, to extract all of the task names:
-
-            >>> evg = EvergreenClient()
-            >>> tasks = evg.get_tasks_from_build_variant('performance_linux_mmap_standalone_9664b4bd7d19144b801767ff8b014520b24a56bc_15_10_21_20_49_17')
-            >>> sorted(tasks.keys())
-            [u'geo', u'insert', u'misc', u'query', u'singleThreaded', u'update', u'where']
-            >>> tasks['geo']['task_id']
-            u'performance_linux_mmap_standalone_geo_9664b4bd7d19144b801767ff8b014520b24a56bc_15_10_21_20_49_17'
-
-        :param build_variant_id: The build ID of a build variant in Evergreen
-        :return: A JSON-compatible dictionary of task information
-        :rtype: dict[str, dict]
+        :param str task_id: The ID of the task in Evergreen
+        :rtype: str
         """
-        response = helpers.get_as_json('{0}/rest/v1/builds/{1}'.format(self.base_url, build_variant_id))
-        return response['tasks']
-
-    def get_test_names_from_task(self, task_id):
-        """Get the tests associated with this task ID.
-
-        Only the test names are returned; other information is stripped away.
-
-        :param task_id: The task ID of a task in Evergreen
-        :return: A list of test names
-        :rtype: list
-        """
-        response = helpers.get_as_json('{0}/rest/v1/tasks/{1}'.format(self.base_url, task_id))
-        return response['test_results'].keys()
-
-    def get_failed_tests(self, task_id):
-        """Get the tests that have failed for this task.
-
-        :param task_id:
-        :return:
-        """
-        response = helpers.get_as_json('{0}/rest/v1/tasks/{1}'.format(self.base_url, task_id))
+        response = self.query_task(task_id)
         if response['aborted']:
             self.logger.warning('Searching for failed tests in a task that has been aborted')
 
-        failed_tests = []
         for test, result in response['test_results'].iteritems():
             if result['status'] != 'pass':
-                failed_tests.append(test)
-        return failed_tests
+                yield test
