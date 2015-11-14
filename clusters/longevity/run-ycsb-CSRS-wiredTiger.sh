ad6324c932edca51d66460da0b6a58af75d03e3b
Chung-Yen Chang <chung-yen.chang@10gen.com>
2015-11-13 22:54:54 -0800

    Fix the path to json file for CSRS longevity

--- clusters/longevity/run-ycsb-CSRS-wiredTiger.sh
+++ clusters/longevity/run-ycsb-CSRS-wiredTiger.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-MC_PER_THREAD_STATS="no" MC_MONITOR_INTERVAL=10 ../../bin/mc -config run-ycsb-wiredTiger-CSRS.json -run ycsb-run-longevity -o perf.json
+MC_PER_THREAD_STATS="no" MC_MONITOR_INTERVAL=10 ../../bin/mc -config run-ycsb-CSRS-wiredTiger.json -run ycsb-run-longevity -o perf.json
