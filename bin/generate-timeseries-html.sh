73e2b3b60f4b7b9dea587d7f14bb38a6c095402d
Henrik Ingo <henrik.ingo@avoinelama.fi>
2016-02-11 14:56:29 +0200

    PERF-375 Automatic Generation of Time Series Charts for Performance Tests
    Adds timeseries.py tool under bin/
    
    Uses it from new script bin/generate-timeseries-html.sh
    
    Generates graphs into reports/graphs/, which becomes part of
    the rerpots.tgz file with logs, that is uploaded to Evergreen.
    
    In a parallel commit, will be called from mongo/etc/system_perf.yml
    and longevity.yml:
    
              ../../bin/retrieve-diag-data.sh
              ../../bin/generate-timeseries-html.sh
    
    Also adds dsi/utils/timeseries-autolauncher.sh that is copied
    into reports.tgz as well. It can be used to launch timeseries.py
    in interactive mode from an unpacked archive.

---
+++ bin/generate-timeseries-html.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source $(dirname $0)/utils.bash
+
+source reports/ips.sh
+
+echo Generate timeseries graphs from FTDC, mongod.log and iostat data.
+
+echo Install python modules needed by timeseries.py into a python virtualenv.
+python --version
+virtualenv ./venv
+source ./venv/bin/activate
+pip install argparse python-dateutil pytz
+
+echo Generate one html file for each host in cluster.
+mkdir -p reports/graphs
+for i in "${ALL_HOST[@]}"
+do
+    python $(dirname $0)/timeseries.py --itz 0 --overview all \
+                                       reports/diag-$i-${!i}/diagnostic.data \
+                                       reports/diag-$i-${!i}/mongod.log \
+                                       --html reports/graphs/timeseries-$i.html
+done
+
+echo
+echo "Done with generating timeseries html files."
+echo "For your convenience, I will also copy the timeseries.py into the tar file."
+echo "It can be launched as a webserver to interactively zoom into the timeseries data."
+echo "Just run timeseries-autolauncher.sh and it will open graphs in Chrome for you."
+echo
+
+cp $(dirname $0)/timeseries.py reports/
+cp $(dirname $0)/../utils/timeseries-autolauncher.sh reports/
+
---
+++ utils/timeseries-autolauncher.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# A helper to launch timeseries.py once for every diagnostics.data stored
+# in this reports.tgz.
+# Note: This script is intended to be copied into and run from inside 
+# the reports.tgz. It will do nothing if run from a dsi repo.
+
+cd $(dirname $0)
+IPS=ips.sh
+
+if [ ! -f $IPS ]
+then
+    echo "This script is intended to be run from inside a reports.tgz archive."
+    echo "This does not appear to be a reports.tgz archive directory. No $IPS file found."
+    exit
+fi
+
+source $IPS
+PORT=8889
+
+echo "Launching timeseries.py $NUM_MONGOD times, once for each diagnostics.data/ in"
+echo "this archive. You can browse the graphs interactively in the chrome tabs that open."
+echo
+echo
+
+virtualenv ./venv
+source ./venv/bin/activate
+pip install argparse python-dateutil pytz
+
+for i in "${ALL_HOST[@]}"
+do
+    python $(dirname $0)/timeseries.py --itz 0 --port $PORT \
+                                       diag-$i-${!i}/diagnostic.data \
+                                       diag-$i-${!i}/mongod.log
+    PORT=$(($PORT+1))
+done
+
