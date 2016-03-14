2aaf9fa32c23d6f9349f498cea0198595f2df68b
Rui Zhang <rui.zhang@mongodb.com>
2016-03-14 16:32:30 -0700

    PERF-377 upgrade terraform

--- bin/setup-dsi-env.sh
+++ bin/setup-dsi-env.sh
@@ -7,7 +7,10 @@ set -v
 mkdir keys
 mkdir terraform
 cd terraform
-wget -nv --no-check-certificate https://s3-us-west-2.amazonaws.com/dsi-donot-remove/terraform/terraform_0.5.3.tar.gz -O - | tar zxv
+curl  -O --retry 10 https://s3-us-west-2.amazonaws.com/dsi-donot-remove/terraform/terraform_0.6.12_linux.tar.gz
+tar zxvf terraform_0.6.12_linux.tar.gz
+mv terraform_0.6.12_linux_amd64/* .
+rm terraform_0.6.12_linux.tar.gz
 
 # install workload wrapper
 cd ../bin
