1fb44b794b00b8c6c13c258b5a488af1ed7171e8
Rui Zhang <rui.zhang@mongodb.com>
2016-04-21 10:45:23 -0700

    PERF-488 add loop to wait for ssd online

--- clusters/windows-single/cluster.tf
+++ clusters/windows-single/cluster.tf
@@ -134,6 +134,8 @@ resource "aws_instance" "member" {
             "chmod +x ~/mongodb/bin/*",
             "mkdir -p ~/bin",
             "ln -s ~/mongodb/bin/mongo ~/bin/mongo",
+            "DRIVE='/cygdrive/y'; until [ -d $DRIVE ]; do echo 'wait for '$DRIVE; sleep 1; done",
+            "DRIVE='/cygdrive/z'; until [ -d $DRIVE ]; do echo 'wait for '$DRIVE; sleep 1; done",
             "echo 'provision done!'",
             # "cd /cygdrive/y ; fio --directory=. --name fio_test_file --direct=1 --rw=randwrite --bs=16k --size=1G --numjobs=16 --time_based --runtime=60 --group_reporting --norandommap --thread",
             # "cd /cygdrive/z ; fio --directory=. --name fio_test_file --direct=1 --rw=randwrite --bs=16k --size=1G --numjobs=16 --time_based --runtime=60 --group_reporting --norandommap --thread",
