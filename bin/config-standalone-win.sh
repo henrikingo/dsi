310f056a0f72aaf2a65b5031f470bd11b7c0e9a0
Rui Zhang <rui.zhang@mongodb.com>
2016-04-23 09:09:12 -0700

    PERF-498 use the right logs path

--- bin/config-standalone-win.sh
+++ bin/config-standalone-win.sh
@@ -159,7 +159,7 @@ startStandalone() {
     runSSHCommand $ssh_url "mkdir -p $JOURNAL_PATH/logs"
     runSSHCommand $ssh_url "cd $MY_ROOT/data; CYGWIN=winsymlinks:native ln -s $DB_PATH/dbs dbs"
     runSSHCommand $ssh_url "cd $MY_ROOT/data/dbs; CYGWIN=winsymlinks:native ln -s $JOURNAL_PATH/journal journal"
-    runSSHCommand $ssh_url "cd $MY_ROOT/data/logs; CYGWIN=winsymlinks:native ln -s $JOURNAL_PATH/logs journal"
+    runSSHCommand $ssh_url "cd $MY_ROOT/data; CYGWIN=winsymlinks:native ln -s $JOURNAL_PATH/logs journal"
 
     if [ $PLATFORM = $WINDOWS_PLATFORM_STRING ]; then
         # install windows service
