ff16dd6f9479e57126d7beb720d86eade98ebe20
Rui Zhang <rui.zhang@mongodb.com>
2016-05-05 14:48:19 -0700

    PERF-494 move mongod instance config to script

--- clusters/replica/cluster.tf
+++ clusters/replica/cluster.tf
@@ -123,30 +123,18 @@ resource "aws_instance" "member" {
     associate_public_ip_address = 1
 
     # We run a remote provisioner on the instance after creating it.
+    provisioner "file" {
+        source = "../remote-scripts/mongod-instance-setup.sh"
+        destination = "/tmp/provision.sh"
+    }
+
     provisioner "remote-exec" {
+        connection {
+            timeoout = "10m"
+        }
         inline = [
-            "sudo yum -y -q install git fio wget sysstat dstat perf xfsprogs",
-            "mkdir mongodb; curl --retry 10 ${var.mongourl} | tar zxv -C mongodb; cd mongodb; mv */bin .; cd ~ ",
-            "mkdir -p ~/bin",
-            "ln -s ~/mongodb/bin/mongo ~/bin/mongo",
-            "dev=/dev/xvdc; sudo umount $dev; sudo mkfs.xfs -f $dev; sudo mount $dev",
-            "sudo chmod 777 /media/ephemeral0",
-            "sudo chown ec2-user /media/ephemeral0",
-            "dev=/dev/xvdd; dpath=/media/ephemeral1; sudo mkdir -p $dpath; sudo umount $dev; sudo mkfs.xfs -f $dev; sudo mount $dev $dpath; ",
-            "sudo chmod 777 /media/ephemeral1",
-            "sudo chown ec2-user /media/ephemeral1",
-            "ln -s /media/ephemeral0 ~/data",
-            "ln -s /media/ephemeral1 ~/journal",
-            "echo 'never' | sudo tee /sys/kernel/mm/transparent_hugepage/enabled",
-            "echo 'never' | sudo tee /sys/kernel/mm/transparent_hugepage/defrag",
-            "echo f | sudo tee /sys/class/net/eth0/queues/rx-0/rps_cpus",
-            "echo f0 | sudo tee /sys/class/net/eth0/queues/tx-0/xps_cpus",
-            "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCmHUZLsuGvNUlCiaZ83jS9f49S0plAtCH19Z2iATOYPH1XE2T8ULcHdFX2GkYiaEqI+fCf1J1opif45sW/5yeDtIp4BfRAdOu2tOvkKvzlnGZndnLzFKuFfBPcysKyrGxkqBvdupOdUROiSIMwPcFgEzyLHk3pQ8lzURiJNtplQ82g3aDi4wneLDK+zuIVCl+QdP/jCc0kpYyrsWKSbxi0YrdpG3E25Q4Rn9uom58c66/3h6MVlk22w7/lMYXWc5fXmyMLwyv4KndH2u3lV45UAb6cuJ6vn6wowiD9N9J1GS57m8jAKaQC1ZVgcZBbDXMR8fbGdc9AH044JVtXe3lT shardtest@test.mongo' | tee -a ~/.ssh/authorized_keys",
-            "fio --directory=/media/ephemeral0 --name fio_test_file --direct=1 --rw=randwrite --bs=16k --size=1G --numjobs=16 --time_based --runtime=60 --group_reporting --norandommap",
-            "fio --directory=/media/ephemeral1 --name fio_test_file --direct=1 --rw=randwrite --bs=16k --size=1G --numjobs=16 --time_based --runtime=60 --group_reporting --norandommap",
-            "rm *.tgz",
-            "rm *.rpm",
-            "ls"
+            "chmod +x /tmp/provision.sh",
+            "/tmp/provision.sh ${var.mongourl}"
         ]
     }
 }
