6e7f2d7f1d2e66589110f24af9560d05ae6a8d7c
dalyd <david.daly@mongodb.com>
2016-05-18 09:32:22 -0400

    PERF-434: Make setup-dsi-env.sh use DSI_PATH if it exists

--- bin/setup-dsi-env.sh
+++ bin/setup-dsi-env.sh
@@ -3,20 +3,25 @@
 set -e
 set -v
 
-# install terraform
-mkdir keys
-mkdir terraform
-cd terraform
+export DSI_PATH=${DSI_PATH:-.}
 
+# install terraform
+mkdir -p keys
+mkdir terraform
+
+cd terraform
 curl  -O --retry 10 https://s3-us-west-2.amazonaws.com/dsi-donot-remove/terraform/terraform_0.6.12_linux.tar.gz
 tar zxvf terraform_0.6.12_linux.tar.gz
 mv terraform_0.6.12_linux_amd64/* .
 rm terraform_0.6.12_linux.tar.gz
+cd ..
 
 # install workload wrapper
-cd ../bin
+pushd .
+cd ${DSI_PATH}/bin
 curl -O --retry 10 https://s3-us-west-2.amazonaws.com/dsi-donot-remove/mc/mc.tar.gz
 tar zxvf mc.tar.gz
 rm mc.tar.gz
-cd ..
+popd
+
 ls
