74cd0b66de42763110f2b9550510becca4ae8469
Shane Harvey <shane.harvey@mongodb.com>
2016-05-31 17:04:16 -0400

    PERF-505 Call update_run_config.sh to update JSON fields

--- bin/run-distributed_correctness_suite_1.sh
+++ bin/run-distributed_correctness_suite_1.sh
@@ -5,6 +5,13 @@ source setting.sh
 
 source ${BINDIR}/setup-workloads.sh
 
+cat ips.sh
+cat run-benchRun.json
+./update_run_config.sh
+cat run-benchRun.json
+rm -rf ./reports
+rm -f ../../reports.tgz
+
 # the current test using bundled shell, if need, we can use following line
 # to speficy a particular version of shell
 # ssh -T -i $PEMFILE $SSHUSER@$mc  "rm -rf 3.1.7; rm -rf bin; mkdir -p 3.1.7; mkdir -p bin; curl https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.1.7.tgz | tar zxv -C 3.1.7; cd 3.1.7; mv */bin .; cd ../bin; ln -s ../3.1.7/bin/mongo mongo"
