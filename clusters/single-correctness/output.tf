4552c31de6b6db8a6d5038ec1689bea8aeb028a9
Rui Zhang <rui.zhang@mongodb.com>
2016-06-09 06:44:52 -0700

    PERF-496 revert correctness cluster
    we need fix other issue first before make this change.

--- clusters/single-correctness/output.tf
+++ clusters/single-correctness/output.tf
@@ -1,15 +1,28 @@
+output "address" {
+  value = "Instances: ${join(", ", aws_instance.member.*.id)}"
+}
+
 output "private_member_ip" {
-  value = "${module.cluster.private_member_ip}"
+  value = "${join(" ", aws_instance.member.*.private_ip)}"
 }
 
 output "public_member_ip" {
-  value = "${module.cluster.public_member_ip}"
+  value = "${join(" ", aws_instance.member.*.public_ip)}"
+}
+
+output "private_mongos_ip" {
+  value = "${aws_instance.master.1.private_ip}"
+}
+
+output "public_mongos_ip" {
+  value = "${aws_instance.master.1.public_ip}"
 }
 
 output "public_ip_mc" {
-  value = "${module.cluster.public_ip_mc}"
+  value = "${aws_instance.master.0.public_ip}"
 }
 
 output "total_count" {
-  value = "${module.cluster.total_count}"
+  value = "${var.count}"
 }
+
