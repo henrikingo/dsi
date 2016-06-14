6c074aaebe19d2f578b4d8ed0bdba2be08cf06f2
Rui Zhang <rui.zhang@mongodb.com>
2016-06-14 13:04:13 -0700

    PERF-497 improve style according to PEP-8 and pylint

--- bin/tests/test_make_terraform_env.py
+++ bin/tests/test_make_terraform_env.py
@@ -2,33 +2,31 @@
 
 from __future__ import print_function
 import unittest
-import sys
-import os
 import datetime
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/common")
-import terraform_config as t  # noqa; pylint: disable=E0401
+from bin.common import terraform_config  # pylint: disable=import-error
 
 
 class TestTerraformConfiguration(unittest.TestCase):
-    """To test terraform configuration class"""
+    """To test terraform configuration class."""
 
     def _test_configuration(self, tf_config, expected_output_string):
         json_string = tf_config.to_json(compact=True)
         self.assertEqual(json_string, expected_output_string)
 
     def test_default(self):
-        """test default terraform configuration, that is to update expire-on only"""
-        tf_config = t.TerraformConfiguration(now=datetime.datetime(2016, 5, 25, 7, 11, 49, 131998))
+        """Test default terraform configuration, that is to update expire-on only."""
+        tf_config = terraform_config.TerraformConfiguration(
+            now=datetime.datetime(2016, 5, 25, 7, 11, 49, 131998))
         json_string = tf_config.to_json(compact=True)
 
         self.assertEqual(json_string, '{"expire_on":"2016-5-26"}')
 
-    def test_normal_cluster_definition(self):
-        """test cluster with proper parameters"""
-        tf_config = t.TerraformConfiguration(topology="test-cluster",
-                                             now=datetime.datetime(2016, 5, 25, 7, 11, 49, 131998))
-
+    def test_mongod_instance(self):
+        """Test mongod instance parameters."""
+        tf_config = terraform_config.TerraformConfiguration(
+            topology="test-cluster",
+            now=datetime.datetime(2016, 5, 25, 7, 11, 49, 131998))
         tf_config.define_mongod_instance(10, "c3.8xlarge")
         self._test_configuration(tf_config,
                                  '{"expire_on":"2016-5-26",'
@@ -38,9 +36,11 @@ class TestTerraformConfiguration(unittest.TestCase):
                                  '"topology":"test-cluster"}')
 
     def test_large_cluster(self):
-        """test cluster with mixed instances"""
-        tf_config = t.TerraformConfiguration(topology="test-cluster",
-                                             now=datetime.datetime(2016, 5, 25, 7, 11, 49, 131998))
+        """Test cluster with mixed instances."""
+        tf_config = terraform_config.TerraformConfiguration(
+            topology="test-cluster",
+            now=datetime.datetime(2016, 5, 25, 7, 11, 49, 131998))
+
         tf_config.define_mongos_instance(1, "m3.2xlarge")
         tf_config.define_mongod_instance(10, "c3.2xlarge")
         tf_config.define_configserver_instance(3, "m3.xlarge")
@@ -58,9 +58,11 @@ class TestTerraformConfiguration(unittest.TestCase):
                                  '"topology":"test-cluster"}')
 
     def test_no_placement_group(self):
-        """test clustenr with placement group"""
-        tf_config = t.TerraformConfiguration(topology="test-cluster",
-                                             now=datetime.datetime(2016, 5, 25, 7, 11, 49, 131998))
+        """Test cluster with placement group."""
+        tf_config = terraform_config.TerraformConfiguration(
+            topology="test-cluster",
+            now=datetime.datetime(2016, 5, 25, 7, 11, 49, 131998))
+
         tf_config.define_mongos_instance(10, "m3.2xlarge")
         self._test_configuration(tf_config,
                                  '{"expire_on":"2016-5-26",'
@@ -70,8 +72,8 @@ class TestTerraformConfiguration(unittest.TestCase):
                                  '"topology":"test-cluster"}')
 
     def test_count_exception(self):
-        """test exception for invalid instance count"""
-        tf_config = t.TerraformConfiguration("test-cluster")
+        """Test exception for invalid instance count."""
+        tf_config = terraform_config.TerraformConfiguration("test-cluster")
         with self.assertRaises(ValueError):
             tf_config.define_mongod_instance(0, "c3.8xlarge")
 
@@ -86,20 +88,23 @@ class TestTerraformConfiguration(unittest.TestCase):
             tf_config.define_configserver_instance(0, "c3.8xlarge")
 
     def test_generate_expire_on_tag(self):
-        """test expire-on tag generator"""
-        tag = t.generate_expire_on_tag(now=datetime.datetime(2016, 5, 25, 7, 11, 49, 131998))
+        """Test expire-on tag generator."""
+        tag = terraform_config.generate_expire_on_tag(
+            now=datetime.datetime(2016, 5, 25, 7, 11, 49, 131998))
         self.assertEqual(tag, "2016-5-26")
 
-        tag = t.generate_expire_on_tag(now=datetime.datetime(2016, 5, 31, 7, 11, 49, 131998))
+        tag = terraform_config.generate_expire_on_tag(
+            now=datetime.datetime(2016, 5, 31, 7, 11, 49, 131998))
         self.assertEqual(tag, "2016-6-1")
 
-        tag = t.generate_expire_on_tag(now=datetime.datetime(2016, 12, 31, 7, 11, 49, 131998))
+        tag = terraform_config.generate_expire_on_tag(
+            now=datetime.datetime(2016, 12, 31, 7, 11, 49, 131998))
         self.assertEqual(tag, "2017-1-1")
 
     def test_placement_group_mapping(self):
-        """test proper mapping from instance type to whether support placement group"""
+        """Test proper mapping from instance type to whether support placement group."""
 
-        self.assertEqual(True, t.support_placement_group("c3.8xlarge"))
-        self.assertEqual(True, t.support_placement_group("m4.xlarege"))
+        self.assertEqual(True, terraform_config.support_placement_group("c3.8xlarge"))
+        self.assertEqual(True, terraform_config.support_placement_group("m4.xlarege"))
 
-        self.assertEqual(False, t.support_placement_group("m3.2xlarege"))
+        self.assertEqual(False, terraform_config.support_placement_group("m3.2xlarege"))
