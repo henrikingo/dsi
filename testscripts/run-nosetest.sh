3f7667f7fae2987b94d31e41579bb22981db215d
Rui Zhang <rui.zhang@mongodb.com>
2016-08-18 13:22:53 -0700

    PERF-591 fix runtests.sh to include bin path

--- testscripts/run-nosetest.sh
+++ testscripts/run-nosetest.sh
@@ -3,4 +3,4 @@
 BUILDIR=$(dirname $0)
 source ${BUILDIR}/test-common.sh
 
-PYTHONPATH=analysis nosetests -v --with-doctest --exe --ignore-files=timeseries.py --ignore-files=update_test_list.py --stop --logging-clear-handlers
+PYTHONPATH=analysis:bin nosetests -v --with-doctest --exe --ignore-files=timeseries.py --ignore-files=update_test_list.py --stop --logging-clear-handlers
