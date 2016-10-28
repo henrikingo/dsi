ebcfc90a7ff13dcb9dc1ae9631cc2415c17ebcde
dalyd <david.daly@mongodb.com>
2016-10-28 14:08:35 -0400

    PERF-771: Enable dbhash check in dsi tests

--- bin/setup-dsi-env.sh
+++ bin/setup-dsi-env.sh
@@ -19,9 +19,7 @@ cp terraform/* work
 # install workload wrapper
 pushd .
 cd ${DSI_PATH}/bin
-curl -O --retry 10 https://s3-us-west-2.amazonaws.com/dsi-donot-remove/mc/mc.tar.gz
-tar zxvf mc.tar.gz
-rm mc.tar.gz
+curl --retry 10 https://s3.amazonaws.com/mciuploads/mission-control/linux/a7be618e77af34471e77d6a82ec4c37d1433c473/mc-mission_control_linux_a7be618e77af34471e77d6a82ec4c37d1433c473_16_10_28_17_55_06.tar.gz | tar -xvz
 popd
 
 ls
