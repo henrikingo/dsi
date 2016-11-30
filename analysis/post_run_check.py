82f33d06451d363cc1492bed44d96eeb5bb88a84
Henrik Ingo <henrik.ingo@mongodb.com>
2016-11-30 16:47:27 +0200

    PERF-781 post_run_check.py: Fix printing of stack trace of caught exception

--- analysis/post_run_check.py
+++ analysis/post_run_check.py
@@ -16,6 +16,7 @@ import json
 import logging
 import StringIO
 import sys
+import traceback
 
 from datetime import timedelta
 import re
@@ -427,7 +428,7 @@ def main(args): # pylint: disable=too-many-locals,too-many-statements,too-many-b
                     'in post_run_check.py: {0}'.format(str(err)))
                 print(sys.exc_info()[0])
                 print(sys.exc_info()[1])
-                print(sys.exc_info()[2].print_exc())
+                traceback.print_tb(sys.exc_info()[2])
                 sys.exit(1)
             if any(val == 'fail' for val in result.itervalues()):
                 result['status'] = 'fail'
