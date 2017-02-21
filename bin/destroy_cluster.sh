06d8a222eacecccd80c79946cc3010f46078b4d2
dalyd <david.daly@mongodb.com>
2017-02-21 15:40:40 -0500

    PERF-860: Terraform destroy needs to be called with --var-file=cluster.json

--- bin/destroy_cluster.sh
+++ bin/destroy_cluster.sh
@@ -3,10 +3,15 @@
 # To destroy a cluster in the current folder.
 cd `dirname $0`
 TERRAFORM="${TERRAFORM:-./terraform}"
-yes yes | $TERRAFORM destroy
+VARFILE=""
+if [ -e cluster.json ]
+then
+    VARFILE="--var-file=cluster.json"
+fi
+yes yes | $TERRAFORM destroy $VARFILE
 
 if [ $? != 0 ]
 then
     # Something maybe wrong, try again
-    yes yes | $TERRAFORM destroy
+    yes yes | $TERRAFORM destroy $VARFILE
 fi
