1be4f9e8812d89fda872db9238315ad45ceca702
Henrik Ingo <henrik.ingo@mongodb.com>
2017-03-06 22:53:06 +0200

    PERF-854 Collect logs also from mongos and configsvr

--- bin/retrieve-diag-data.sh
+++ bin/retrieve-diag-data.sh
@@ -10,7 +10,7 @@ mkdir -p reports
 
 for i in "${ALL_HOST[@]}"
 do
-    echo "Retrieve diag data from ${i}:${!i}"
+    echo "Retrieve diag data from mongod ${i}:${!i}"
     mkdir -p reports/diag-$i-${!i}
 
     for f in "${FILES[@]}"
@@ -18,5 +18,24 @@ do
         scpFile ${!i} $f reports/diag-$i-${!i}/
     done
 done
+
+# For sharded variants, also collect log data for mongos and config servers
+if [ "$ms" != "" ]
+then
+    echo "Retrieve diag data from mongos: $ms"
+    mkdir -p reports/diag-mongos-$ms
+    # No diagnostic.data on mongos
+    scpFile $ms "data/logs/mongo*.log" reports/diag-mongos-$ms/ || true
+
+    for c in $config1 $config2 $config3
+    do
+        echo "Retrieve diag data from config server: $c"
+        mkdir -p reports/diag-config-$c
+        for f in "${FILES[@]}"
+        do
+            scpFile $c $f reports/diag-config-$c/ || true
+        done
+    done
+fi
 
 echo "Done save diag data"
