bb8436986a43911b73df7cf87788b75b2d39287c
Jim OLeary <jim.oleary@gmail.com>
2017-05-17 15:14:42 +0100

    PERF-974: remove scripts fix import in test_alias

--- bin/tests/test_alias.py
+++ bin/tests/test_alias.py
@@ -7,7 +7,7 @@ import unittest
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from aliases import unalias, expand, ALIASES
+from alias import unalias, expand, ALIASES
 
 
 class AliasTestCase(unittest.TestCase):
