0b072be0b0d2a82657d64562432d6a753739dec0
Rushil Kumar <rushil.kumar@10gen.com>
2017-06-12 15:18:37 -0400

    PERF 938: Switched to pip yamllint. Using a strict configuration file.

--- testscripts/lint-yml.sh
+++ testscripts/lint-yml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CMD="/opt/chef/embedded/bin/yaml-lint $(find . -name '*.yml')"
+CMD="yamllint $(find . -name '*.yml' ! -path './tests/unittest-files/perf.yml' ! -path './tests/unittest-files/system_perf.yml' ! -path './tests/unittest-files/valid_evergreen.yml')"
 
 echo "Linting yaml files"
 echo $CMD
