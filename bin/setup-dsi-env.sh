0d33f7972e3fc1427372b5501e82320bde9eff75
Vick Mena <vickmena@gmail.com>
2017-11-15 17:14:15 -0500

    PERF-1156: SERVER-30770 Cleanup changes in DSI

--- bin/setup-dsi-env.sh
+++ bin/setup-dsi-env.sh
@@ -5,25 +5,6 @@ set -v
 
 export DSI_PATH=${DSI_PATH:-.}
 
-EVG_DATA_DIR="/data/infrastructure_provisioning"
-if [ -d "$EVG_DATA_DIR" ]; then
-	# If terraform-provider-docker is found, that means the older version
-	# of terraform is being used since the new binary has everything packaged
-	# into one.
-	if [ -e $EVG_DATA_DIR/terraform/terraform-provider-docker ]; then
-        # This only sets the TERRAFORM for all child processes.
-        # Therefore it does not need to be set back to the original value.
-		# infrastructure_teardown.sh should specifically use the old
-		# terraform in EVG_DATA_DIR since the new terraform is not
-		# backwards compatible
-		export TERRAFORM=$EVG_DATA_DIR/terraform/terraform
-		# Teardown the infrastructure since the new terraform binary will
-		# not work properly with the old terraform files. Also remove
-		# the old saved state once teardown is complete
-		$EVG_DATA_DIR/terraform/infrastructure_teardown.sh && rm -rf "$EVG_DATA_DIR"
-	fi
-fi
-
 # install terraform
 mkdir terraform
 
@@ -46,8 +27,3 @@ sysctl -w vm.overcommit_memory=1
 sysctl vm.overcommit_memory
 
 ls
-
-# Install pip modules with virtualenv
-virtualenv ./venv
-source ./venv/bin/activate
-pip install -r $DSI_PATH/requirements.txt
