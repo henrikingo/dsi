c1d4b392e5be404c2b9f37ed71bc68ea9dc7a77c
Ryan Timmons <119094+rtimmons@users.noreply.github.com>
2018-02-26 13:42:40 -0500

    PERF-1320: Don't Archive venv Directory (#33)

--- bin/make_artifact.sh
+++ bin/make_artifact.sh
@@ -1,34 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Make the test log artifact. Assumes it is run in the working directory. If dsi-artifacts.tgz
 # already exists, will delete and recreate
 
+# fail whole script for undefined vars or any command failure
+set -eou pipefail
+
 source ./dsienv.sh
 
-# Exclude security sensitive files
-TAR_EXCLUDE="--exclude runtime_secret.yml --exclude security.tf --exclude aws_ssh_key.pem"
+# Exclude security-sensitive and unnecessary files
+TAR_EXCLUDE=""
+TAR_EXCLUDE="$TAR_EXCLUDE --exclude runtime_secret.yml --exclude security.tf --exclude aws_ssh_key.pem"
+TAR_EXCLUDE="$TAR_EXCLUDE --exclude venv --exclude reports.tgz"
 
-TAR_ARTIFACT=dsi-artifacts.tgz
+TAR_ARTIFACT="dsi-artifacts.tgz"
+
 echo "Creating $TAR_ARTIFACT"
-rm $TAR_ARTIFACT >/dev/null 2>&1 || true
-tar $TAR_EXCLUDE --exclude reports.tgz -zcf $TAR_ARTIFACT *
 
-
+rm "$TAR_ARTIFACT" >/dev/null 2>&1 || true
+tar $TAR_EXCLUDE -zcf "$TAR_ARTIFACT" ./*
 
 # Kept for system_perf.yml backward compatibility (SERVER-32896). Safe to remove after 4.0 release.
 if [ -e reports.tgz ]; then
     echo "Reports.tgz already exists. Not generating"
 else
     mkdir -p ./reports
-    cd reports
     # move additional file here
-    cp ../infrastructure_provisioning.out.yml . || true
-    cp ../bootstrap.yml . || true
-    cp ../runtime.yml . || true
+    cp infrastructure_provisioning.out.yml ./reports || true
+    cp bootstrap.yml                       ./reports || true
+    cp runtime.yml                         ./reports || true
+    cp perf.json                           ./reports || true
 
-    if [ -f "../terraform.log" ]; then cp ../terraform.log .; fi
-    cp ../perf.json . || true
-    cd ..
+    if [ -f "terraform.log" ]; then
+        cp terraform.log ./reports
+    fi
 
     tar -hzcf reports.tgz reports
 fi
