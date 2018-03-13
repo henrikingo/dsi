843eebfcbf7f9f5e7a116a1f23f2b46f11988d30
Henrik Ingo <henrik.ingo@mongodb.com>
2018-03-13 12:27:55 +0200

    PERF-1303 Remove analysis/ from coverage as it doesn't work after all.

--- testscripts/run-nosetest.sh
+++ testscripts/run-nosetest.sh
@@ -3,11 +3,12 @@
 BUILDIR=$(dirname $0)
 source ${BUILDIR}/test-common.sh
 
-COVER_PACKAGES="analysis aws_tools bin"
+COVER_PACKAGES="aws_tools bin"
 COVERAGE="--with-coverage --cover-inclusive  --cover-xml --cover-xml-file=coverage.xml"
 for package in $COVER_PACKAGES
 do
   COVERAGE="$COVERAGE --cover-package=$package"
 done
+
 
 PYTHONPATH=analysis:bin nosetests -v --exe $COVERAGE --logging-clear-handlers $@
