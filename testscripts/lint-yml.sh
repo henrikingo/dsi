04d122ff3823a0354606ae04167242da640e6d35
piakochar <piakochar11@gmail.com>
2018-03-28 16:31:43 -0400

    PERF-1287: added config check to lint-yml (#62)

--- testscripts/lint-yml.sh
+++ testscripts/lint-yml.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+#yamllint linting
 CMD="yamllint $(find . -name '*.yml' ! -path './tests/unittest-files/perf.yml' ! -path './tests/unittest-files/system_perf.yml' ! -path './tests/unittest-files/valid_evergreen.yml') .yamllint"
 
 echo "Linting yaml files"
 echo $CMD
 $CMD
+
+#config file linting
+FILES="$(find ./configurations -name '*.yml')"
+echo $FILES
+
+failed=0
+
+for I in $FILES; do
+	python ./bin/common/config.py "$I"
+	if [ $? -ne 0 ]; then
+		failed=$((failed + 1))
+	fi
+done
+
+exit $failed
