7c3ad3a872a550b40a6d0bf3e73dfd8fc7e2a252
Jim O'Leary <jim.oleary@gmail.com>
2018-04-06 16:41:54 +0100

    PERF-1422: capture more detailed fio metrics

--- bin/fio-test.sh
+++ bin/fio-test.sh
@@ -7,4 +7,6 @@ HOSTNAME=$1
 scp -o StrictHostKeyChecking=no fio.ini ${HOSTNAME}:./
 ssh -A ${HOSTNAME} "mkdir -p ./data/fio && fio --output-format=json --output=fio.json fio.ini"
 scp ${HOSTNAME}:./fio.json .
-ssh -A ${HOSTNAME} "rm -r data/fio"
+ssh -A ${HOSTNAME} "tar czvf fio_results.tgz fio*.log"
+scp ${HOSTNAME}:./fio_results.tgz .
+ssh -A ${HOSTNAME} "rm -r data/fio fio_results.tgz fio*.log"
