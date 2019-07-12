b1de0408c0505db91c250aca67c31c0b29f47334
David <david.daly@mongodb.com>
2019-07-12 07:59:16 -0400

    Tig 1680 Add Ability to set device readahead in DSI (#438)

--- terraform/remote-scripts/system-setup.sh
+++ terraform/remote-scripts/system-setup.sh
@@ -137,13 +137,6 @@ if [[ "${WITH_EBS}" == "with_seeded_ebs" ]]; then
 fi
 
 
-# mongodb production recommended configuration
-echo 'never' | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
-echo 'never' | sudo tee /sys/kernel/mm/transparent_hugepage/defrag
-echo f | sudo tee /sys/class/net/eth0/queues/rx-0/rps_cpus
-echo f0 | sudo tee /sys/class/net/eth0/queues/tx-0/xps_cpus
-
-
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCmHUZLsuGvNUlCiaZ83jS9f49S0plAtCH19Z2iATOYPH1XE2T8ULcHdFX2GkYiaEqI+fCf1J1opif45sW/5yeDtIp4BfRAdOu2tOvkKvzlnGZndnLzFKuFfBPcysKyrGxkqBvdupOdUROiSIMwPcFgEzyLHk3pQ8lzURiJNtplQ82g3aDi4wneLDK+zuIVCl+QdP/jCc0kpYyrsWKSbxi0YrdpG3E25Q4Rn9uom58c66/3h6MVlk22w7/lMYXWc5fXmyMLwyv4KndH2u3lV45UAb6cuJ6vn6wowiD9N9J1GS57m8jAKaQC1ZVgcZBbDXMR8fbGdc9AH044JVtXe3lT shardtest@test.mongo' | tee -a ~/.ssh/authorized_keys
 
 
