e3bdc53f52cba55a65eb2c022c51a2a5d650d084
Henrik Ingo <henrik.ingo@mongodb.com>
2020-03-19 23:47:37 +0200

    PERF-1407 Run shellcheck on embedded scripts in configurations yaml

--- bin/fio-test.sh
+++ bin/fio-test.sh
@@ -6,8 +6,8 @@ HOSTNAME=$1
 
 # Heuristic to detect Atlas clusters from the hostname, which have the form
 # *.mongodb-dev.net or *.mongodb.net
-domain=$(echo $HOSTNAME | cut -d. -f2)
-tld=$(echo $HOSTNAME | cut -d. -f3)
+domain=$(echo "$HOSTNAME" | cut -d. -f2)
+tld=$(echo "$HOSTNAME" | cut -d. -f3)
 
 if [ "$tld" == "net" ]
 then
@@ -18,9 +18,9 @@ then
   fi
 fi
 
-scp -o StrictHostKeyChecking=no fio.ini ${HOSTNAME}:./
-ssh -A ${HOSTNAME} "mkdir -p ./data/fio && fio --output-format=json --output=fio.json fio.ini"
-scp ${HOSTNAME}:./fio.json .
-ssh -A ${HOSTNAME} "tar czvf fio_results.tgz fio*.log"
-scp ${HOSTNAME}:./fio_results.tgz .
-ssh -A ${HOSTNAME} "rm -r data/fio fio_results.tgz fio*.log"
+scp -o StrictHostKeyChecking=no fio.ini "${HOSTNAME}":./
+ssh -A "${HOSTNAME}" "mkdir -p ./data/fio && fio --output-format=json --output=fio.json fio.ini"
+scp "${HOSTNAME}":./fio.json .
+ssh -A "${HOSTNAME}" "tar czvf fio_results.tgz fio*.log"
+scp "${HOSTNAME}":./fio_results.tgz .
+ssh -A "${HOSTNAME}" "rm -r data/fio fio_results.tgz fio*.log"
--- bin/iperf-test.sh
+++ bin/iperf-test.sh
@@ -7,8 +7,8 @@ THIS_HOST=$(hostname -i)
 
 # Heuristic to detect Atlas clusters from the hostname, which have the form
 # *.mongodb-dev.net or *.mongodb.net
-domain=$(echo $HOSTNAME | cut -d. -f2)
-tld=$(echo $HOSTNAME | cut -d. -f3)
+domain=$(echo "$HOSTNAME" | cut -d. -f2)
+tld=$(echo "$HOSTNAME" | cut -d. -f3)
 
 if [ "$tld" == "net" ]
 then
@@ -23,22 +23,22 @@ fi
 echo "Run iperf network tests between $THIS_HOST and $HOSTNAME."
 
 sudo rm -f iperf.json
-ssh -A ${HOSTNAME} "sudo pkill -15 iperf3" || true
+ssh -A "${HOSTNAME}" "sudo pkill -15 iperf3" || true
 sudo pkill -15 iperf3 || true
 
 # Start the remote process
 echo "Start the iperf listener side process"
 LISTENER_CMD="sudo /usr/local/bin/iperf3 -s -p 27016 -D"
-ssh -A ${HOSTNAME} "$LISTENER_CMD" &
+ssh -A "${HOSTNAME}" "$LISTENER_CMD" &
 
 sleep 2
 
 echo "Start iperf client side process"
-sudo /usr/local/bin/iperf3 -c ${HOSTNAME} -i 2 -t 60 -V -p 27016 -J --logfile iperf.json
+sudo /usr/local/bin/iperf3 -c "${HOSTNAME}" -i 2 -t 60 -V -p 27016 -J --logfile iperf.json
 
 echo "Clean up iperf processes"
 kill %1
-ssh -A ${HOSTNAME} "sudo pkill iperf3" || true
+ssh -A "${HOSTNAME}" "sudo pkill iperf3" || true
 sudo pkill -15 iperf3 || true
 
 echo "Done $0"
